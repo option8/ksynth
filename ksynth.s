99e24d9a06c3d8f1733c084c232ef0b956c15ee7
Charles Mangin <option8@option8.com>
2016-12-27 22:26:58 -0500

    updated merlin32 source
    added midi lookup table

--- ksynth.s
+++ ksynth.s
@@ -6,8 +6,8 @@ MULTIPLIER		EQU		$FC					; duration multiplier (defaults to $ff)
 OSCILLATOR		EQU		$FD					; second oscillator value - decrements once for vibrato
 OSCCOUNTER		EQU		$FE					; tracks second oscillator value
 SEQUENCE		EQU		$FF					; note number/address offset from song origin
+MIDILOOKUP		EQU		$1100				; MIDI style note lookup table
 SONGORIGIN		EQU		$1000				; song header start address
-MIDILOOKUP		EQU		$1100				; MIDI notes lookup table address
 CLICKBIT1		EQU		$031E				; modified byte - set to FF for rest note
 CLICKBIT2		EQU		$0313
 
@@ -70,4 +70,11 @@ NEXTNOTE		INC		SEQUENCE			; increment to next note address
 				LDA		SEQUENCE			; load Accumulator with next note address
 				BNE		]LOOP				; branch to LOOP
 ENDSONG 		RTS							; 
+	
+	
+				ORG		$1130											; MIDI lookup table
+				
+				HEX		00,00,00,FC,EF,E1,D5,C9,BD,B3,A9,9F,96,8E,86,7E	; $1130
+				HEX		77,71,6A,64,5E,59,54,4F,4B,47,43,3F,3C,38,35,32	; $1140
+				HEX		2F,2D,2A,27,25,23,21,1F,1E,1C,1A,19,17,16,15,13	; $1150
 			
